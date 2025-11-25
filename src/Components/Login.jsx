fbbea38403f3a11c462c1446f57a552711a856f6
Anuragkite <Anuragkite143@gmail.com>
2025-11-25 21:42:18 +0530

    tried to add the firebase but no success and also added the created the validation for login form

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,46 +1,100 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Header from "./Header";
+import { CheckValidData } from "../utils/Validate";
 
 const Login = () => {
   const [isSignInForm, setSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const email = useRef();
+  const password = useRef();
+  const name = useRef();
 
   const ToggleSignInForm = () => {
     setSignInForm(!isSignInForm);
   };
 
+  const handleButtonClick = () => {
+
+
+    const emailValue = email.current.value;
+    const passwordValue = password.current.value;
+    let  nameValue = null ;
+
+    if(!isSignInForm){
+      nameValue = name.current.value;
+    }
+
+
+   
+    // validate
+    const message = CheckValidData(emailValue, passwordValue,nameValue);
+    setErrorMessage(message);
+
+    if (!message) {
+      console.log("Form submitted successfully!");
+    }
+  };
+
   return (
     <div>
       <Header />
-      <div className="absolute">
-        <img src="https://assets.nflxext.com/ffe/siteui/vlv3/6fd9d446-cd78-453a-8c9c-417ed3e00422/web/IN-en-20251117-TRIFECTA-perspective_2fe4e381-977f-49fd-a7f4-1da0bcf09429_large.jpg "></img>
+
+      {/* Background */}
+      <div className="absolute inset-0 -z-10">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/6fd9d446-cd78-453a-8c9c-417ed3e00422/web/IN-en-20251117-TRIFECTA-perspective_2fe4e381-977f-49fd-a7f4-1da0bcf09429_large.jpg"
+          className="w-full h-full object-cover"
+          alt="bg"
+        />
       </div>
-      <form className="absolute bg-black opacity-80 h-8/12 w-3/12 p-10 right-3/8 top-2/12   flex flex-col  rounded-4xl">
-        <h1 className="font-bold text-4xl font-sans  text-white mx-3 my-5">
+
+      {/* Login Form */}
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="absolute bg-black/70 w-1/4 p-10 top-1/4 left-1/2 -translate-x-1/2 flex flex-col rounded-2xl text-white"
+      >
+        <h1 className="font-bold text-4xl mb-5">
           {isSignInForm ? "Sign In" : "Sign Up"}
         </h1>
-       {!isSignInForm && (<input
-          type="text"
-          placeholder="Full Name  "
-          className="  opacity-100  text-white my-3 p-6 rounded-2xl bg-gray-700"
-        />)}
+
+        {!isSignInForm && (
+          <input
+          ref={name}
+            type="text"
+            placeholder="Full Name"
+            className="my-3 p-4 rounded-xl bg-gray-700"
+          />
+        )}
+
         <input
+          ref={email}
           type="text"
-          placeholder="Email Address  "
-          className="  opacity-100  text-white my-3 p-6 rounded-2xl bg-gray-700"
+          placeholder="Email Address"
+          className="my-3 p-4 rounded-xl bg-gray-700"
         />
+
         <input
+          ref={password}
           type="password"
-          placeholder=" Password "
-          className=" opacity-100  text-white my-3 p-6 rounded-2xl  bg-gray-700 "
-        ></input>
-        <button className="my-3 p-4 bg-red-700 opacity-100  cursor-pointer rounded text-white">
+          placeholder="Password"
+          className="my-3 p-4 rounded-xl bg-gray-700"
+        />
+
+        {/* Validation Error */}
+        {errorMessage && (
+          <p className="text-red-500 font-semibold">{errorMessage}</p>
+        )}
+
+        <button
+          onClick={handleButtonClick}
+          className="my-4 p-4 bg-red-700 rounded-xl"
+        >
           {isSignInForm ? "Sign In" : "Sign Up"}
         </button>
-        <p className="py-2 text-white opacity-100 cursor-pointer" onClick={ToggleSignInForm}>
-          {" "}
-          {isSignInForm
-            ? "Not Registered ? Sign up.."
-            : "Already a User ? Sign In.." }
+
+        <p onClick={ToggleSignInForm} className="cursor-pointer">
+          {isSignInForm ? "Not Registered? Sign Up" : "Already a user? Sign In"}
         </p>
       </form>
     </div>
