a381c821f4346a63a5db5dbe569fc7f629ff742a
Anuragkite <Anuragkite143@gmail.com>
2025-11-27 15:52:12 +0530

    added the sign out btn w/ working login in browse page and also in the home page

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import Login from "./Login";
 import Header from "./Header";
 import Browse from "./Browse";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, useLocation } from "react-router-dom";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../utils/firebase";
 import { useDispatch } from "react-redux";
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
-
+import React from "react";
+import { signOut } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useLocation, useNavigate } from "react-router-dom";
 const Header = () => {
-  return (
-    <div className='absolute w-full h-24 bg-gradient-to-b from-black to-transparent z-10'>
-        <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt='Netflix-logo' className='w-30 p-2 border border-black '></img>
-    </div>
-  )
-}
+  const Auth = auth;
+  const navigate = useNavigate();
+ const location = useLocation();
 
-export default Header
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        console.log("sign Out successfully !");
+        navigate("/");
+
+      })
+      .catch((error) => {
+        // An error happened.
+        console.log("Error in Sign-Out , here is Error :", error);
+      });
+  };
+
+  return (
+    <div className="absolute w-full h-24 bg-gradient-to-b from-black to-transparent z-10 flex  justify-between">
+      <img
+        id="NetFlix Logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+        alt="Netflix-logo"
+        className="w-30 p-2 border border-black "
+      ></img>
+     {location.pathname ==="/browse" ? <div className="p-3 w-45 h-20 m-3 flex justify-evenly ">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png?20201013161117" />
+        <button onClick={handleSignOut} className="font-bold text-white ">
+          (Sign Out)
+        </button>
+      </div>:<></>}
+    </div>
+  );
+};
+
+export default Header;
